09e2b568e4b06aece43f632a7b62bfee8b7140a1
Long Nguyen <longnewint@gmail.com>
2025-03-02 21:50:39 -0500

    Add js

---
+++ main.js
@@ -0,0 +1,42 @@
+
+
+const addTask = document.querySelector('#add-task');
+const taskContainer = document.querySelector('#task-container');
+const inputTask = document.querySelector('#input-task');
+
+addTask.addEventListener('click', () => {
+  let task = document.createElement('div');
+  task.classList.add('task')
+
+  let li = document.createElement('li');
+  li.innerText = inputTask.value;
+  task.appendChild(li);
+
+  let checkButton = document.createElement("button");
+  checkButton.innerHTML = '<i class="fa-solid fa-check"></i>';
+  checkButton.classList.add('checkTask');
+  task.appendChild(checkButton);
+  
+  let deleteButton = document.createElement("button");
+  deleteButton.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+  deleteButton.classList.add('deleteTask');
+  task.appendChild(deleteButton);
+
+  if(inputTask.value === "") {
+    alert('Please enter a Task!');
+  } else {
+    taskContainer.appendChild(task);
+  }
+
+  inputTask.value = "";
+
+  checkButton.addEventListener('click', () => {
+    checkButton.parentElement.style.textDecoration = "line-through";
+  })
+
+  deleteButton.addEventListener('click', (e) => {
+    let target = e.target;
+
+    target.parentElement.parentElement.remove();
+  })
+});
