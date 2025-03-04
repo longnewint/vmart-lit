7516f5bcfdcdcb33319abc85e88e35a96fa7b6a8
Long Nguyen <longnewint@gmail.com>
2025-03-03 23:11:31 -0500

    Test fetch from button click

--- main.js
+++ main.js
@@ -1,42 +1,11 @@
+import "@ui5/webcomponents-fiori/dist/ShellBar.js";
+import "@ui5/webcomponents-fiori/dist/ShellBarSpacer.js";
 
+import "@ui5/webcomponents/dist/Button.js";
 
-const addTask = document.querySelector('#add-task');
-const taskContainer = document.querySelector('#task-container');
-const inputTask = document.querySelector('#input-task');
+const testButton = document.querySelector('#test-btn');
 
-addTask.addEventListener('click', () => {
-  let task = document.createElement('div');
-  task.classList.add('task')
-
-  let li = document.createElement('li');
-  li.innerText = inputTask.value;
-  task.appendChild(li);
-
-  let checkButton = document.createElement("button");
-  checkButton.innerHTML = '<i class="fa-solid fa-check"></i>';
-  checkButton.classList.add('checkTask');
-  task.appendChild(checkButton);
-  
-  let deleteButton = document.createElement("button");
-  deleteButton.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
-  deleteButton.classList.add('deleteTask');
-  task.appendChild(deleteButton);
-
-  if(inputTask.value === "") {
-    alert('Please enter a Task!');
-  } else {
-    taskContainer.appendChild(task);
-  }
-
-  inputTask.value = "";
-
-  checkButton.addEventListener('click', () => {
-    checkButton.parentElement.style.textDecoration = "line-through";
-  })
-
-  deleteButton.addEventListener('click', (e) => {
-    let target = e.target;
-
-    target.parentElement.parentElement.remove();
-  })
+testButton.addEventListener('click', async () => {
+  const response = await fetch('http://localhost:8080/hello');
+  console.log(response.text());
 });
