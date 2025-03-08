9b2b7b62de5abd4af04fbf8e03559d2325c781fc
Long Nguyen <longnewint@gmail.com>
2025-03-07 23:36:24 -0500

    Add html element vmt-nav-bar

--- main.js
+++ main.js
@@ -1,16 +1,60 @@
 import {html, css, LitElement} from 'lit';
-import {customElement, property} from 'lit/decorators.js';
+import "@ui5/webcomponents/dist/Button.js";
 
-@customElement('vmt-nav-bar')
 export class NavBar extends LitElement {
   static styles = css`
-  p { color: blue }
+  .nav-bar-1 ul {
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    background-color: white;
+    list-style: none;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+  }
+  nav li {
+    height: 50px;
+  }
+  nav a {
+    height: 100%;
+    padding: 0 30px;
+    text-decoration: none;
+    display: flex;
+    align-items: center;
+    color: black;
+  }
+
+  nav a:hover {
+    background-color: grey;
+  }
+
+  nav .left-cluster {
+    margin-right: auto;
+  }
   `;
 
-  @property()
-  name = 'Somebody';
+  static properties = {
+    name: {type: String},
+  };
+
+  constructor() {
+    super();
+    this.name = 'Somebody';
+  }
 
   render() {
-    return html`<p>Hello, ${this.name}!</p>`;
+    return html`
+    <nav class='nav-bar-1'>
+      <div>
+      <ul>
+        <li class='left-cluster'><a href=''>vMart</a></li>
+        <li><a href=''>Button1</a></li>
+        <a href=''><button>Sign In</button></a>
+      </ul>
+      </div>
+    </nav>
+    `;
   }
 }
+customElements.define('vmt-nav-bar', NavBar);
